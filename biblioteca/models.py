d7285fd5a4195b6aefad1675e2b200f203f62d61
Ismael Braian Cayampi Segovia <brian.toshka@gmail.com>
2023-05-17 14:43:25 -0300

    se agregp el modelo socio

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Socio(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    fecha_nacimiento=models.DateField
+    activo=models.BooleanField(default=True)
