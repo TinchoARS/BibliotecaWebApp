53205c5a4f553944a0831fac3ae9b6b36e7a246d
TinchoARS <fradejas84@gmail.com>
2023-05-17 19:37:01 -0300

    modelo_autor

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    nacionalidad=models.CharField(max_length=60)
+    activo=models.BooleanField(default=True)
