1871adc9277f32fd6f90c05381cc50d25bee9c2d
Loboxos <vcristian528@gmail.com>
2023-05-17 12:27:04 -0300

    "modelo agregado"

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    titulo=models.CharField(max_length=30)
+    descripcion=models.CharField(max_length=255)
+    isbn=models.IntegerField()
+    autor=models.ForeignKey(Autor,related_name="autor",on_delete=models.CASCADE)
+    activo=models.BooleanField(default=True)
