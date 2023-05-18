801ef4e5803c3f624172e29ed592cac7a1d7e9bb
Ismael Braian Cayampi Segovia <brian.toshka@gmail.com>
2023-05-18 10:08:49 -0300

    arreglo modelo socio

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -16,6 +16,6 @@ class Libro(models.Model):
 class Socio(models.Model):
     nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
-    fecha_nacimiento=models.DateField
+    fecha_nacimiento=models.DateField()
     activo=models.BooleanField(default=True)
 
