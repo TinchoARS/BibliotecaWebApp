7727877fd4fe639827939e745fdd755e21e5e81d
Loboxos <vcristian528@gmail.com>
2023-05-18 09:42:30 -0300

    related_name arreglado

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -10,7 +10,7 @@ class Libro(models.Model):
     titulo=models.CharField(max_length=30)
     descripcion=models.CharField(max_length=255)
     isbn=models.IntegerField()
-    autor=models.ForeignKey(Autor,related_name="autor",on_delete=models.CASCADE)
+    autor=models.ForeignKey(Autor,related_name="libros",on_delete=models.CASCADE)
     activo=models.BooleanField(default=True)
 
 class Socio(models.Model):
