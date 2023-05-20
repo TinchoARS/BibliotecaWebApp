fa93ea4efe1b61c3c4439a040e1f16e791baafbc
TinchoARS <fradejas84@gmail.com>
2023-05-19 23:12:02 -0300

    view_url_activar

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import saludar,desactivar_empleado
+from .views import saludar,desactivar_empleado, activar_empleado
 
 urlpatterns = [
     path('example/', saludar, name='saludar'),
     # Add more URL patterns here
     path('empleados/desactivar/<int:id>', desactivar_empleado, name='desactivar_empleado'),
+    path('empleados/activar/<int:id>', activar_empleado, name='activar_empleado'),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -16,3 +16,15 @@ def desactivar_empleado(request, id):
         mensaje= "Empleado ya esta desactivado"
     
     return render(request, 'desactivar_empleado.html',{'mensaje':mensaje})
+
+def activar_empleado(request, id):
+    empleado = get_object_or_404(Empleado, id=id)
+
+    if not empleado.activo:
+        empleado.activo = True
+        empleado.save()
+        mensaje = "Empleado activado correctamente."
+    else:
+        mensaje = "Empleado ya está activo."
+
+    return render(request, 'mensaje_activacion.html', {'mensaje': mensaje})
