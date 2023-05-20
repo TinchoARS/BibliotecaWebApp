21d7a85a4ac23da80a3737ec56a2b5f13322c081
TinchoARS <fradejas84@gmail.com>
2023-05-19 22:57:43 -0300

    view_url_desactivar

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import saludar
+from .views import saludar,desactivar_empleado
 
 urlpatterns = [
     path('example/', saludar, name='saludar'),
     # Add more URL patterns here
+    path('empleados/desactivar/<int:id>', desactivar_empleado, name='desactivar_empleado'),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,get_object_or_404, HttpResponse
+from biblioteca.models import Empleado
 
 # Create your views here.
 def saludar(request):
     return HttpResponse("hola")
+
+def desactivar_empleado(request, id):
+    Empleado= get_object_or_404(Empleado, id=id)
+    
+    if Empleado.activo:
+        Empleado.activo=False
+        Empleado.save()
+        mensaje= "Empleado desactivado correctamente"
+    else:
+        mensaje= "Empleado ya esta desactivado"
+    
+    return render(request, 'desactivar_empleado.html',{'mensaje':mensaje})
