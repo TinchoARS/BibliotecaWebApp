9a1de96476fe4597526a6ee75113154ca7dd0477
TinchoARS <fradejas84@gmail.com>
2023-05-21 11:53:42 -0300

    urls actualizadas y form de nuevos empleados

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import saludar,registrar_empleado
+from .views import saludar,registrar_empleado, desactivar_empleado, activar_empleado
 
 urlpatterns = [
     path('example/', saludar, name='saludar'),
-    path('empleados/nuevo',registrar_empleado, name='registrar_empleado')
+    path('empleados/nuevo',registrar_empleado, name='registrar_empleado'),
+     path('empleados/desactivar/<int:id>', desactivar_empleado, name='desactivar_empleado'),
+    path('empleados/activar/<int:id>', activar_empleado, name='activar_empleado')
 ]
