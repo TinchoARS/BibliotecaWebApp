93ed0a16ed9c14f147a169e4fc138a61f3440878
TinchoARS <fradejas84@gmail.com>
2023-05-21 12:01:07 -0300

    views actualizadas

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,9 +1,38 @@
+
+from django.shortcuts import render,get_object_or_404, HttpResponse
+
 from django.shortcuts import render,HttpResponse
+
 from biblioteca.models import Empleado
 
 # Create your views here.
 def saludar(request):
     return HttpResponse("hola")
+
+
+def desactivar_empleado(request, id):
+    Empleado= get_object_or_404(Empleado, id=id)
+    
+    if Empleado.activo:
+        Empleado.activo=False
+        Empleado.save()
+        mensaje= "Empleado desactivado correctamente"
+    else:
+        mensaje= "Empleado ya esta desactivado"
+    
+    return render(request, 'desactivar_empleado.html',{'mensaje':mensaje})
+
+def activar_empleado(request, id):
+    empleado = get_object_or_404(Empleado, id=id)
+
+    if not empleado.activo:
+        empleado.activo = True
+        empleado.save()
+        mensaje = "Empleado activado correctamente."
+    else:
+        mensaje = "Empleado ya está activo."
+
+    return render(request, 'mensaje_activacion.html', {'mensaje': mensaje})
 
 def registrar_empleado(request):
     listadoDeEmpleados=Empleado.objects.all()
@@ -21,3 +50,4 @@ def registrar_empleado(request):
         numero_legajo=numeroLeg_empleado
         )
     return render(request,"biblioteca/registrar_empleado.html",context)
+
