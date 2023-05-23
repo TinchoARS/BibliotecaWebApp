af0158deceda0328b6fd1b97e07572d298cadcef
Loboxos <vcristian528@gmail.com>
2023-05-23 20:35:20 -0300

    arreglos de errores en paths

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('empleados/desactivar/<int:id>', desactivar_empleado, name='desactivar_empleado'),
     path('empleados/activar/<int:id>', activar_empleado, name='activar_empleado'),
     path('empleados/listado/', listado_empleados, name='listado_empleados'),
-    path('empleados/actualizar/<int:empleado_id>', actualizar_datos_empleado, name='listado_empleados')
+    path('empleados/actualizar/<int:empleado_id>', actualizar_datos_empleado, name='actualizar_empleado')
 ]
