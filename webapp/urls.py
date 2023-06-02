95a32d7282a96a16d29c82ac79fe2e00239fc168
TinchoARS <fradejas84@gmail.com>
2023-06-02 16:45:16 -0300

    arreglo de api

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('biblioteca/',include("biblioteca.urls")),
-    path('api', include("api.urls")),
+    path('api/', include("api.urls")),
 ]
