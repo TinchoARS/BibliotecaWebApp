557e5d6235f5d0f9b98f70cc34e4fb1e97b5c859
TinchoARS <fradejas84@gmail.com>
2023-06-02 16:25:46 -0300

    api

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('biblioteca/',include("biblioteca.urls")),
+    path('api', include("api.urls")),
 ]
